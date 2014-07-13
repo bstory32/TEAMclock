cc5633bab4482951d6c251ff0869aa4b45795490
Neil Sammons <neil.a.sammons@gmail.com>
2014-07-13 16:21:28 -0500

    created manage-account.php

---
+++ manage-account.php
@@ -0,0 +1,13 @@
+<?php include ('shared/header.php') ?>
+      <div class="container">
+<div class="row">
+	<div class="col-sm-6">
+		<h1>Manage Account</h1>
+	</div>
+	<div class="col-sm-6">
+		
+	</div>
+</div>
+
+      </div><!-- /.container -->
+<?php include ('shared/footer.php') ?>R
