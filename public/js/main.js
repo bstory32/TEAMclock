c67cbbb1b726305fb58edba00d1355e3b2ac54d9
Brian Story <bstory32@gmail.com>
2014-07-13 11:38:17 -0500

    cleaned up project

--- public/js/main.js
+++
@@ -1,8 +0,0 @@
-var $projectItem = $('.projects-section');
-
-
-
-$projectItem.find('[context="editDetails"]').on('click', function(){
-	$(this).parent().parent().find('.edit-memo').slideToggle();
-
-});
