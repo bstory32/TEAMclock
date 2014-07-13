66394a65c45aa40d0bb997928fe76fe7474240e2
Brian Story <bstory32@gmail.com>
2014-07-13 17:57:47 -0500

    added content to index, fixed header logo font

--- index.php
+++ index.php
@@ -3,10 +3,47 @@
 <div class="about-teamclock">
 	<div class="container">
 		<div class="row">
-			<h1>Team Clock helps you manage your project hours</h1>
-			<p>
-
-			</p>
+			<div class="col-sm-6">
+				<h1>Team Clock helps you manage your project hours free</h1>
+				<ul>
+					<li>
+						Easily manage team members hours and hourly wage
+					</li>
+					<li>
+						Easily know how much money you earned within a project
+					</li>
+					<li>
+						Keep track of what was done and how many hours it took
+					</li>
+				</ul>
+			</div>
+			<div class="col-sm-6">
+				<h2>Start managing your projects now</h2>
+				<a href="signup.php" class="btn btn-success">Sign up free</a>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="uses-teamclock">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h2>Who uses teamclock?</h2>
+				<ul class="list-inline">
+					<li>
+						<img src="http://placehold.it/150x150">
+					</li>
+					<li>
+						<img src="http://placehold.it/150x150">
+					</li>
+					<li>
+						<img src="http://placehold.it/150x150">
+					</li>
+					<li>
+						<img src="http://placehold.it/150x150">
+					</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </div>
