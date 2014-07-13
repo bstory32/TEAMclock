0af3c8886798b2c6a6b1b0f135ce9d583a7ec32b
Neil Sammons <neil.a.sammons@gmail.com>
2014-07-13 15:42:41 -0500

    created manage-project.php

---
+++ manage-project.php
@@ -0,0 +1,10 @@
+<?php include ('shared/header.php') ?>
+<div class="container">
+<div class="row manage-project-panel">
+	<div class="col-sm-6">
+		<h1>Manage Project</h1>
+	</div>
+</div>
+
+</div><!-- /.container -->
+<?php include ('shared/footer.php') ?>
