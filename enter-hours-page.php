82d0668097da47c0794f428fdcdc8f26434583a2
Neil Sammons <neil.a.sammons@gmail.com>
2014-07-13 16:15:22 -0500

    added enter-hours-page.php

---
+++ enter-hours-page.php
@@ -0,0 +1,49 @@
+<?php include ('shared/header.php') ?>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12 enter-hours-header">
+			<h1>MuddRunner</h1>
+			<button type="button" class="btn btn-success">Add Hours</button>
+
+		</div>
+	</div>
+	<div class="enter-hours-list">
+		<ul>
+			<li>
+				<div class="row">
+					<div class="col-sm-2">
+						<h3>Enter Hours</h3>
+						<span>0.00</span>
+					</div> 
+					<div class="col-sm-10">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, vitae laudantium natus, odit sit reprehenderit ratione obcaecati eum laboriosam, autem ad aliquam perspiciatis consequatur nobis repudiandae, neque voluptas placeat sunt.</p>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="row">
+					<div class="col-sm-2">
+						<h3>Enter Hours</h3>
+						<span>0.00</span>
+					</div> 
+					<div class="col-sm-10">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, vitae laudantium natus, odit sit reprehenderit ratione obcaecati eum laboriosam, autem ad aliquam perspiciatis consequatur nobis repudiandae, neque voluptas placeat sunt.</p>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="row">
+					<div class="col-sm-2">
+						<h3>Enter Hours</h3>
+						<span>0.00</span>
+					</div> 
+					<div class="col-sm-10">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, vitae laudantium natus, odit sit reprehenderit ratione obcaecati eum laboriosam, autem ad aliquam perspiciatis consequatur nobis repudiandae, neque voluptas placeat sunt.</p>
+					</div>
+				</div>
+			</li>
+		</ul>
+	</div>
+	<button type="button" class="btn btn-success">Submit Hours</button>
+</div><!-- /.container -->
+<?php include ('shared/footer.php') ?>
