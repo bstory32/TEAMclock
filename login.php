5808b5703b36c0a1bb9c939bce36151a8eb5ffb4
Neil Sammons <neil.a.sammons@gmail.com>
2014-07-17 20:45:08 -0500

    added color scheme to login.php

--- login.php
+++ login.php
@@ -6,30 +6,35 @@
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-sm-6 login-panel">
-			<h1>Sign in to your Account</h1>
-			<hr>
-			<div class="form-container">
-				<form role="form">
-					<div class="form-group">
-						<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-					</div>
-					<div class="form-group">
-						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-					</div>
-					<div class="checkbox">
-						<label>
-							<input type="checkbox"> Stay logged in
-						</label>
-						<a href="#">Forget password?</a>
-					</div>
-					<button type="button" class="btn btn-primary btn-lg">Login</button>
-				</form>
+		<div class="col-sm-6 ">
+			
+			
+			<div class="login-panel">
+				<h1>Sign in to your Account</h1>
+				<div class="form-container">
+					<form role="form">
+						<div class="form-group">
+							<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+						</div>
+						<div class="form-group">
+							<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+						</div>
+						<div class="checkbox">
+							<label>
+								<input type="checkbox"> Stay logged in
+							</label>
+							<a href="#">Forget password?</a>
+						</div>
+						<button type="button" class="btn btn-primary btn-lg">Login</button>
+					</form>
+				</div>
 			</div>
 		</div>
-		<div class="col-sm-6 not-registered-panel">
-			<h1>Not a Member?</h1>
-			<button type="button" class="btn btn-primary">Get registered today!</button>
+		<div class="col-sm-6 ">
+			<div class="not-registered-panel">
+				<h1>Not a member yet?</h1>
+				<button type="button" class="btn btn-primary">Sign up now</button>
+			</div>
 		</div>
 	</div>
 
