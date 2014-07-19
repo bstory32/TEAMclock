ef0aacbf8b0c5baf8baa9c3a0dc524b65eac1f56
Neil Sammons <neil.a.sammons@gmail.com>
2014-07-19 12:53:05 -0500

    added styling to registration page

--- registration.php
+++ registration.php
@@ -1,26 +1,33 @@
 <?php include ('shared/header.php') ?>
 <div class="container">
 	<div class="row">
-		<div class="col-sm-6 login-panel">
-			<h1>Register new Account</h1>
-			<form role="form">
-				<div class="form-group">
-					<label for="exampleInputEmail1">Email address</label>
-					<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-				</div>
-				<div class="checkbox">
-					<label>
-						<input type="checkbox"> Stay logged in
-					</label>
-				</div>
-				<button type="submit" class="btn btn-default">Register</button>
-			</form>
+		<div class="col-sm-6">
+
+			<div class="existing-member-panel">
+				<h1>Already a Member?</h1>
+				<button type="button" class="btn btn-primary">Sign In</button>
+			</div>
+
 		</div>
-		<div class="col-sm-4 col-sm-offset-2 existing-member-panel">
-			<h1>Already a Member?</h1>
-			<button type="button" class="btn btn-primary">Sign In</button>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque magnam eos temporibus accusantium amet, nesciunt saepe neque facilis, molestiae odio! Officiis odio adipisci, aliquam! Eaque, debitis. Labore doloremque, fuga ducimus.</p>
+		<div class="col-sm-6">
+
+			<div class="registration-panel">
+				<h1>Register new Account</h1>
+				<form role="form">
+					<div class="form-group">
+						<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+					</div>
+					<div class="checkbox">
+						<label>
+							<input type="checkbox"> Stay logged in
+						</label>
+					</div>
+					<button type="submit" class="btn btn-default">Register</button>
+				</form>
+			</div>
+
 		</div>
+		
 	</div>
 
 
