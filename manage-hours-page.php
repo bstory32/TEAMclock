9b8cb22dc826d3a9b2421199aa09e558f5fc4f19
Neil Sammons <neil.a.sammons@gmail.com>
2014-07-20 17:08:28 -0500

    Added modal styling to project

--- manage-hours-page.php
+++ manage-hours-page.php
@@ -38,6 +38,22 @@
 				</div>
 			</li>
 		</ul>
+		
+	</div>
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="pagination">
+				<ul class="list-inline">
+					<li><a href="#">Current Week</a></li>
+					<li><a href="#">Week 25</a></li>
+					<li><a href="#">Week 24</a></li>
+					<li><a href="#">Week 23</a></li>
+					<li><a href="#">Week 22</a></li>
+					<li>...</li>
+					<li><a href="#">Week 1</a></li>
+				</ul>
+			</div>
+		</div>
 	</div>
 </div><!-- /.container -->
 
@@ -53,11 +69,15 @@
 				<form role="form">
 					<div class="form-group">
 						<label for="day">Day</label>
-						<input type="email" class="form-control" id="day" placeholder="10/5/14">
+						<input type="date" class="form-control" id="day" placeholder="10/5/14">
 					</div>
 					<div class="form-group">
 						<label for="day">hours</label>
-						<input type="email" class="form-control" id="hours" placeholder="0.00 hrs">
+						<input type="number" class="form-control" id="hours" placeholder="0.00 hrs">
+					</div>
+					<div class="form-group">
+						<label for="day">Comment</label>
+						<textarea name="comment" id="" width="100%"></textarea>					
 					</div>
 				</form>
 			</div>
