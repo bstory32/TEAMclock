076ffcc8e6a0bee3861a1afd0e9df9f2f70122d5
Neil Sammons <neil.a.sammons@gmail.com>
2014-07-20 16:43:57 -0500

    added styling to manage-hours-page

---
+++ manage-hours-page.php
@@ -0,0 +1,43 @@
+<?php include ('shared/header.php') ?>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12 manage-hours-header">
+			<h1>MuddRunner</h1>
+			<button type="button" class="btn btn-success">Add Hours</button>
+
+		</div>
+	</div>
+	<div class="manage-hours-list">
+		<ul>
+			<li>
+				<div class="row">
+					<div class="col-sm-3 ">
+						<div class="date">
+							<h3>Tuesday 14th</h3>
+							<span class="hours">2hrs 30min</span>
+							<a href="#">Edit Hours</a>
+						</div>
+					</div> 
+					<div class="col-sm-9">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, vitae laudantium natus, odit sit reprehenderit ratione obcaecati eum laboriosam, autem ad aliquam perspiciatis consequatur nobis repudiandae, neque voluptas placeat sunt.</p>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="row">
+					<div class="col-sm-3 ">
+						<div class="date">
+							<h3>Monday 13th</h3>
+							<span class="hours">2hrs 30min</span>
+							<a href="#">Edit Hours</a>
+						</div>
+					</div> 
+					<div class="col-sm-9">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, vitae laudantium natus, odit sit reprehenderit ratione obcaecati eum laboriosam, autem ad aliquam perspiciatis consequatur nobis repudiandae, neque voluptas placeat sunt.</p>
+					</div>
+				</div>
+			</li>
+		</ul>
+	</div>
+</div><!-- /.container -->
+<?php include ('shared/footer.php') ?>
